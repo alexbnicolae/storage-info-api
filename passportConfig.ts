c8d0588e3f7c07b73b7bf617a069bc9389bdf53b
alexbnicolae <boranescu.alex@yahoo.com>
2023-10-22 04:42:47 +0300

    env login url added

--- passportConfig.ts
+++ passportConfig.ts
@@ -6,13 +6,13 @@ export const facebook:  any = {
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret:  process.env.FACEBOOK_APP_SECRET,
   //todo: based on env, change url to localhost, dev or prod
-  callbackURL: "https://data-wallet-server.herokuapp.com/user/login/facebook/callback",
+  callbackURL: `${process.env.LOGIN_URL}/user/login/facebook/callback`,
 };
 export const google: any = {
   clientID: process.env.GMAIL_APP_ID,
   clientSecret: process.env.GMAIL_APP_SECRET,
   //todo: based on env, change url to localhost, dev or prod
-  callbackURL: "https://data-wallet-server.herokuapp.com/user/login/google/callback",
+  callbackURL: `${process.env.LOGIN_URL}/user/login/google/callback`,
 };
 // export const google: any = {
 //   clientID: process.env.GMAIL_APP_ID,
@@ -25,7 +25,7 @@ export const google: any = {
 export const github: StrategyOptions = {
   clientID: process.env.GITHUB_APP_ID ?? '',
   clientSecret: process.env.GITHUB_APP_SECRET ?? '',
-  callbackURL: "https://data-wallet-server.herokuapp.com/user/login/github/callback",
+  callbackURL: `${process.env.LOGIN_URL}/user/login/github/callback`,
   scope: ['user:email']
 
 };
